0318cdd8674d3140e135a922c6fa74c553b46842
OMulari <omulari24@student.oulu.fi>
2025-01-23 23:58:09 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.querySelector('button');
+    button.addEventListener('click', function () {
+        alert('You clicked the button!');
+    });
+});
+
+
+
 function scrollToSection(id) {
     const section = document.getElementById(id);
     section.scrollIntoView({ behavior: 'smooth' });
 }
+
