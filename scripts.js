afc4dea9d4d5604916cd0996b81dcf625e5ad8f3
OMulari <omulari24@student.oulu.fi>
2025-01-29 19:20:49 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -95,91 +95,120 @@ window.onload = function () {
 
 
 
+
 const translations = {
-    "~CerebraStyles~": "~CerebraStyles~",
-    "-Your Digital Vision, Tailored to Reality": "-Sinun digitaalinen visio, räätälöity todellisuuteen",
+    "CerebraStyles": "CerebraStyles",
+    "-Your Digital Vision, Tailored to Reality": "-Digitaalinen visiosi, räätälöity todellisuuteen",
     "Us": "Meistä",
-    "Design": "Suunnittelu",
+    "Design": "Toteutus",
+    "Crew": "Tiimi",
     "Contact": "Yhteystiedot",
-    "Where Requirements Meet Passion": "Vaatimukset kohtaavat intohimon",
+    "Requirements Meet Passion": "Vaatimukset kohtaavat intohimon",
     "• At CerebraStyles, we deliver tailored websites (UI/UX) to help you stand out and make lasting impressions on others.": 
-    "• CerebraStyles tarjoaa räätälöityjä verkkosivustoja (UI/UX), jotka jättävät vaikutuksen muihin.",
-    "• We believe in sustainable and innovative web designs come from blending customer-centric requirements with our passion for development.": 
-    "• Uskomme, että kestävät sekä innovatiiviset verkkosivustot syntyvät, kun yhdistämme asiakaskeskeiset vaatimukset ja meidän intohimo kehittämiseen.",
+    "• CerebraStyles suunnittelee räätälöityjä UI/UX-verkkosivustoja, joissa luovuus kohtaa intohimon ja asiakkaan vaatimukset.",
+    "• We believe that sustainable and innovative web designs come from blending customer centric requirements with our passion for development.": 
+    "• Uskomme, että kestävät ja innovatiiviset verkkosivut syntyvät, kun yhdistämme nämä ominaisuudet yhteen kokonaisuuteen.",
     "• At the moment we mainly specialize in smaller projects, such as portfolios, landing pages, and personal brands, to ensure precise and refined results, completed within your timeframe.": 
     "• Tällä hetkellä erikoistumme etenkin pienempiin projekteihin, kuten portfolioihin, aloitussivuihin ja henkilöbrändeihin, varmistaaksemme tarkat ja viimeistellyt tulokset.",
-    "Our Work": "Työmme",
     "• Our commitment to detail derives not just from client requirements but from our obsession to improve and create unique projects.": 
-    "• Omistautumisemme yksityiskohtiin ei johdu vain asiakkaiden vaatimuksista vaan myös pakkomielteestämme kehittyä ja luoda ainutlaatuisia projekteja.",
+    "• Emme keskity yksityiskohtiin vain asiakkaiden odotusten vuoksi, vaan siksi, että intohimomme kehitykseen ja ainutlaatuisten projektien luomiseen on rajaton.",
     "• Every project is an opportunity for us to push our limits and ensure we deliver something better than ever before.": 
     "• Jokainen projekti on mahdollisuus meille ylittää rajamme ja toimittaa jotain parempaa kuin koskaan aiemmin.",
-    "• During our partnership, our team will collaborate closely with you, delivering thoughtful designs that meet your needs and reflect your vision.": 
-    "• Kumppanuutemme aikana tiimimme tekee tiivistä yhteistyötä kanssasi ja tarjoaa huolellisesti suunniteltuja ratkaisuja.",
+    "• During our partnership, our team will work closely with you in a relaxed yet focused manner, delivering thoughtful designs that meet your needs and reflect your vision.": 
+    "• Kumppanuutemme aikana tiimimme työskentelee rennolla mutta määrätietoisella otteella, tehden tiivistä yhteistyötä kanssasi ja tarjoten harkittuja suunnitteluratkaisuja, jotka vastaavat tarpeitasi ja heijastavat visiotasi.",
     "Start Your Journey,": "Aloita matkasi,"
 };
 
 const originalTexts = {
-    "~CerebraStyles~": "~CerebraStyles~",
-    "-Sinun digitaalinen visio, räätälöity todellisuuteen": "-Your Digital Vision, Tailored to Reality",
+    "CerebraStyles": "CerebraStyles",
+    "-Digitaalinen visiosi, räätälöity todellisuuteen": "-Your Digital Vision, Tailored to Reality",
     "Meistä": "Us",
-    "Suunnittelu": "Design",
+    "Toteutus": "Design",
+    "Tiimi": "Crew",
     "Yhteystiedot": "Contact",
-    "Vaatimukset kohtaavat intohimon": "Where Requirements Meet Passion",
-    "• CerebraStyles tarjoaa räätälöityjä verkkosivustoja (UI/UX), jotka jättävät vaikutuksen muihin.": 
+    "Vaatimukset kohtaavat intohimon": "Requirements Meet Passion",
+    "• CerebraStyles suunnittelee räätälöityjä UI/UX-verkkosivustoja, joissa luovuus kohtaa intohimon ja asiakkaan vaatimukset.": 
     "• At CerebraStyles, we deliver tailored websites (UI/UX) to help you stand out and make lasting impressions on others.",
-    "• Uskomme, että kestävät sekä innovatiiviset verkkosivustot syntyvät, kun yhdistämme asiakaskeskeiset vaatimukset ja meidän intohimo kehittämiseen.": 
-    "• We believe in sustainable and innovative web designs come from blending customer-centric requirements with our passion for development.",
+    "• Uskomme, että kestävät ja innovatiiviset verkkosivut syntyvät, kun yhdistämme nämä ominaisuudet yhteen kokonaisuuteen.": 
+    "• We believe that sustainable and innovative web designs come from blending customer centric requirements with our passion for development.",
     "• Tällä hetkellä erikoistumme etenkin pienempiin projekteihin, kuten portfolioihin, aloitussivuihin ja henkilöbrändeihin, varmistaaksemme tarkat ja viimeistellyt tulokset.": 
     "• At the moment we mainly specialize in smaller projects, such as portfolios, landing pages, and personal brands, to ensure precise and refined results, completed within your timeframe.",
-    "Työmme": "Our Work",
-    "• Omistautumisemme yksityiskohtiin ei johdu vain asiakkaiden vaatimuksista vaan myös pakkomielteestämme kehittyä ja luoda ainutlaatuisia projekteja.": 
+    "Toteutus": "Design",
+    "• Emme keskity yksityiskohtiin vain asiakkaiden odotusten vuoksi, vaan siksi, että intohimomme kehitykseen ja ainutlaatuisten projektien luomiseen on rajaton.": 
     "• Our commitment to detail derives not just from client requirements but from our obsession to improve and create unique projects.",
     "• Jokainen projekti on mahdollisuus meille ylittää rajamme ja toimittaa jotain parempaa kuin koskaan aiemmin.": 
     "• Every project is an opportunity for us to push our limits and ensure we deliver something better than ever before.",
-    "• Kumppanuutemme aikana tiimimme tekee tiivistä yhteistyötä kanssasi ja tarjoaa huolellisesti suunniteltuja ratkaisuja.": 
-    "• During our partnership, our team will collaborate closely with you, delivering thoughtful designs that meet your needs and reflect your vision.",
+    "• Kumppanuutemme aikana tiimimme työskentelee rennolla, mutta määrätietoisella otteella, tehden tiivistä yhteistyötä kanssasi, tarjoten huolellisesti suunniteltuja ratkaisuja.": 
+    "• During our partnership, our team will work closely with you in a relaxed yet focused manner, delivering thoughtful designs that meet your needs and reflect your vision.",
     "Aloita matkasi,": "Start Your Journey,"
+
 };
 
 let isTranslated = false;
 
-document.getElementById("translateButton").addEventListener("click", () => {
-    if (!isTranslated) {
-        // Translate to Finnish
-        document.getElementById("mainTitle").textContent = translations["~CerebraStyles~"];
-        document.getElementById("tagline").textContent = translations["-Your Digital Vision, Tailored to Reality"];
-        document.getElementById("navAbout").textContent = translations["Us"];
-        document.getElementById("navServices").textContent = translations["Design"];
-        document.getElementById("navContact").textContent = translations["Contact"];
-        document.getElementById("aboutHeading").textContent = translations["Where Requirements Meet Passion"];
-        document.getElementById("aboutText1").textContent = translations["• At CerebraStyles, we deliver tailored websites (UI/UX) to help you stand out and make lasting impressions on others."];
-        document.getElementById("aboutText2").textContent = translations["• We believe in sustainable and innovative web designs come from blending customer-centric requirements with our passion for development."];
-        document.getElementById("aboutText3").textContent = translations["• At the moment we mainly specialize in smaller projects, such as portfolios, landing pages, and personal brands, to ensure precise and refined results, completed within your timeframe."];
-        document.getElementById("servicesHeading").textContent = translations["Our Work"];
-        document.getElementById("servicesText1").textContent = translations["• Our commitment to detail derives not just from client requirements but from our obsession to improve and create unique projects."];
-        document.getElementById("servicesText2").textContent = translations["• Every project is an opportunity for us to push our limits and ensure we deliver something better than ever before."];
-        document.getElementById("servicesText3").textContent = translations["• During our partnership, our team will collaborate closely with you, delivering thoughtful designs that meet your needs and reflect your vision."];
-        document.getElementById("contactHeading").textContent = translations["Start Your Journey,"];
-        isTranslated = true;
-        document.getElementById("translateButton").textContent = "Translate to English";
-    } else {
-        // Translate back to English
-        document.getElementById("mainTitle").textContent = originalTexts["~CerebraStyles~"];
-        document.getElementById("tagline").textContent = originalTexts["-Sinun digitaalinen visio, räätälöity todellisuuteen"];
-        document.getElementById("navAbout").textContent = originalTexts["Meistä"];
-        document.getElementById("navServices").textContent = originalTexts["Suunnittelu"];
-        document.getElementById("navContact").textContent = originalTexts["Yhteystiedot"];
-        document.getElementById("aboutHeading").textContent = originalTexts["Vaatimukset kohtaavat intohimon"];
-        document.getElementById("aboutText1").textContent = originalTexts["• CerebraStyles tarjoaa räätälöityjä verkkosivustoja (UI/UX), jotka jättävät vaikutuksen muihin."];
-        document.getElementById("aboutText2").textContent = originalTexts["• Uskomme, että kestävät sekä innovatiiviset verkkosivustot syntyvät, kun yhdistämme asiakaskeskeiset vaatimukset ja meidän intohimo kehittämiseen."];
-        document.getElementById("aboutText3").textContent = originalTexts["• Tällä hetkellä erikoistumme etenkin pienempiin projekteihin, kuten portfolioihin, aloitussivuihin ja henkilöbrändeihin, varmistaaksemme tarkat ja viimeistellyt tulokset.."];
-        document.getElementById("servicesHeading").textContent = originalTexts["Työmme"];
-        document.getElementById("servicesText1").textContent = originalTexts["• Omistautumisemme yksityiskohtiin ei johdu vain asiakkaiden vaatimuksista vaan myös pakkomielteestämme kehittyä ja luoda ainutlaatuisia projekteja."];
-        document.getElementById("servicesText2").textContent = originalTexts["• Jokainen projekti on mahdollisuus meille ylittää rajamme ja toimittaa jotain parempaa kuin koskaan aiemmin."];
-        document.getElementById("servicesText3").textContent = originalTexts["• Kumppanuutemme aikana tiimimme tekee tiivistä yhteistyötä kanssasi ja tarjoaa huolellisesti suunniteltuja ratkaisuja."];
-        document.getElementById("contactHeading").textContent = originalTexts["Aloita matkasi,"];
-        isTranslated = false;
-        document.getElementById("translateButton").textContent = "Finnish";
-    }
-});
+
+
+        document.getElementById("translateButton").addEventListener("click", () => {
+            if (!isTranslated) {
+                // Translate to Finnish
+                document.getElementById("mainTitle").textContent = translations["CerebraStyles"];
+                document.getElementById("tagline").textContent = translations["-Your Digital Vision, Tailored to Reality"];
+                document.getElementById("navAbout").textContent = translations["Us"];
+                document.getElementById("navServices").textContent = translations["Design"];
+                document.getElementById("navCrew").textContent = translations["Crew"];
+        
+                document.getElementById("navContact").textContent = translations["Contact"];
+                document.getElementById("aboutHeading").textContent = translations["Requirements Meet Passion"];
+                document.getElementById("aboutText1").textContent = translations["• At CerebraStyles, we deliver tailored websites (UI/UX) to help you stand out and make lasting impressions on others."];
+               
+                
+                document.getElementById("aboutText22").textContent = translations["• We believe that sustainable and innovative web designs come from blending customer centric requirements with our passion for development."];
+                document.getElementById("aboutText3").textContent = translations["• At the moment we mainly specialize in smaller projects, such as portfolios, landing pages, and personal brands, to ensure precise and refined results, completed within your timeframe."];
+                document.getElementById("servicesHeading").textContent = translations["Design"];
+                document.getElementById("servicesText1").textContent = translations["• Our commitment to detail derives not just from client requirements but from our obsession to improve and create unique projects."];
+                document.getElementById("servicesText2").textContent = translations["• Every project is an opportunity for us to push our limits and ensure we deliver something better than ever before."];
+                document.getElementById("servicesText3").textContent = translations["• During our partnership, our team will work closely with you in a relaxed yet focused manner, delivering thoughtful designs that meet your needs and reflect your vision."];
+                document.getElementById("contactHeading").textContent = translations["Start Your Journey,"];
+        
+                isTranslated = true;
+                document.getElementById("translateButton").textContent = "English";
+            } else {
+                // Translate back to English
+                document.getElementById("mainTitle").textContent = originalTexts["CerebraStyles"];
+                document.getElementById("tagline").textContent = originalTexts["-Digitaalinen visiosi, räätälöity todellisuuteen"];
+                document.getElementById("navAbout").textContent = originalTexts["Meistä"];
+                document.getElementById("navServices").textContent = originalTexts["Toteutus"];
+                document.getElementById("navCrew").textContent = originalTexts["Tiimi"];
+        
+                document.getElementById("navContact").textContent = originalTexts["Yhteystiedot"];
+                document.getElementById("aboutHeading").textContent = originalTexts["Vaatimukset kohtaavat intohimon"];
+                document.getElementById("aboutText1").textContent = originalTexts["• CerebraStyles suunnittelee räätälöityjä UI/UX-verkkosivustoja, joissa luovuus kohtaa intohimon ja asiakkaan vaatimukset."];
+               
+                
+                document.getElementById("aboutText22").textContent = originalTexts["• Uskomme, että kestävät ja innovatiiviset verkkosivut syntyvät, kun yhdistämme nämä ominaisuudet yhteen kokonaisuuteen."];
+                document.getElementById("aboutText3").textContent = originalTexts["• Tällä hetkellä erikoistumme etenkin pienempiin projekteihin, kuten portfolioihin, aloitussivuihin ja henkilöbrändeihin, varmistaaksemme tarkat ja viimeistellyt tulokset."];
+                document.getElementById("servicesHeading").textContent = originalTexts["Toteutus"];
+                document.getElementById("servicesText1").textContent = originalTexts["• Emme keskity yksityiskohtiin vain asiakkaiden odotusten vuoksi, vaan siksi, että intohimomme kehitykseen ja ainutlaatuisten projektien luomiseen on rajaton."];
+                document.getElementById("servicesText2").textContent = originalTexts["• Jokainen projekti on mahdollisuus meille ylittää rajamme ja toimittaa jotain parempaa kuin koskaan aiemmin."];
+                document.getElementById("servicesText3").textContent = originalTexts["• Kumppanuutemme aikana tiimimme työskentelee rennolla, mutta määrätietoisella otteella, tehden tiivistä yhteistyötä kanssasi, tarjoten huolellisesti suunniteltuja ratkaisuja."];
+                document.getElementById("contactHeading").textContent = originalTexts["Aloita matkasi,"];
+                document.getElementById("navCrew").textContent = originalTexts["Tiimi"];
+
+        
+                isTranslated = false;
+                document.getElementById("translateButton").textContent = "Finnish";
+            }
+        
+        
+        
+        
+        
+            document.getElementById("navTeam").addEventListener("click", (event) => {
+                event.preventDefault();
+                document.querySelectorAll(".content-section").forEach((section) => {
+                    section.classList.add("hidden"); 
+                });
+                document.getElementById("team").classList.remove("hidden");
+            });
+        });
 
